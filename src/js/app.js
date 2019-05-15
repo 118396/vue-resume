515d2f3f6bf2a804cdabee714365b22bffa844c8
weihu <1183969348@qq.com>
2019-05-15 20:34:48 +0800

    添加打印功能

--- src/js/app.js
+++ src/js/app.js
@@ -180,6 +180,9 @@ let app = new Vue({
         removeProject(index) {
             this.resume.projects.splice(index, 1)
         },
+        print(){
+            window.print();
+        }
     }
 })
 
